3dd2ff07e03d8df638947d18fcaf94f92ad77d7b
Adamski <Kamil.Adamski@unilever.com>
2019-11-22 17:39:12 +0100

    Creating reducer logic

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,8 +1,37 @@
-import {NEW_MESSAGE,SET_USERNAME} from './types';
+import {NEW_MESSAGE,SET_USERNAME,REACTION_OBJECTS} from './types';
 
-export const initialState = {messages: [],username:'anonymous'};
+export const initialState = {
+    messages: [],
+    username:'anonymous',
+    reactionMap:{}
+};
+
+const REACTION_TYPES=REACTION_OBJECTS.map(
+    REACTION_OBJECT=>REACTION_OBJECT.type
+);
 
 const reducer=(state,action)=>{
+if (REACTION_TYPES.includes(action.type)){
+    let reactionMap;
+    const {messageId}=action.item;
+    const messageReactions=state.reactionMap[messageId];
+
+    if(messageId){
+        reactionMap={
+            ...state.reactionMap,
+            [messageId]:[...messageReactions, action.item]
+        }
+    }else{
+        reactionMap={
+            ...state.reactionMap,
+            [messageId]:[action.item]
+        }
+    }
+
+    return{...state,reactionMap};
+}
+
+
     switch(action.type){
         case NEW_MESSAGE:
             return{...state,messages:[...state.messages,action.item]};
